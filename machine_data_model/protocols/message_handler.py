b48e113417d8fee1339c048fd5354e51c6e86a69
pt199vr <pt99vr@gmail.com>
2024-12-20 11:19:22 +0100

    A template for message handler

---
+++ machine_data_model/protocols/message_handler.py
@@ -0,0 +1,54 @@
+from machine_data_model.protocols.glacier_v1.enumeration_for_messages import MessageType
+from machine_data_model.protocols.glacier_v1.message import Message
+from machine_data_model.protocols.glacier_v1.method_message import MethodCall
+from machine_data_model.protocols.glacier_v1.variable_message import (
+    VariableCall,
+    VarOperation,
+)
+from typing import Any
+
+
+class MessageHandler:
+    message: Message
+
+    def set_message(self, mex: Message) -> None:
+        self.message = mex
+
+    def get_message(self) -> Message:
+        return self.message
+
+    def has_message(self) -> bool:
+        return self.message is not None
+
+    def handle_message(self, node_list: list[Any]) -> list[Any]:
+        result: list[Any] = []
+        if not self.has_message():
+            return result
+        topology = self.message.topology
+        if not (topology == MessageType.REQUEST):
+            return result
+
+        payload = self.message.payload
+
+        if isinstance(payload, VariableCall):
+            mex_type: VarOperation = payload.operation
+            operation = 0
+            varname = payload.varname
+
+            if varname in node_list:
+                match mex_type:
+                    case VarOperation.READ:
+                        operation = VarOperation.READ.value
+                    case VarOperation.WRITE:
+                        operation = VarOperation.WRITE.value
+                    case VarOperation.SUBSCRIBE:
+                        operation = VarOperation.SUBSCRIBE.value
+                    case VarOperation.UNSUBSCRIBE:
+                        operation = VarOperation.UNSUBSCRIBE.value
+
+                result = [operation, varname, payload.args]
+
+        elif isinstance(payload, MethodCall) and payload.method in node_list:
+            result = [payload.method, payload.args]
+
+        return result
