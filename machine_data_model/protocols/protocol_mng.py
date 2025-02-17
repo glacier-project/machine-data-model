b05abf8df0d75d0e2e477645665c06377a326a3d
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2025-02-17 13:25:27 -0500

    Add comments to Message and ProtocolMng classes.

--- machine_data_model/protocols/protocol_mng.py
+++ machine_data_model/protocols/protocol_mng.py
@@ -5,33 +5,51 @@ from dataclasses import dataclass
 
 @dataclass(init=True, slots=True)
 class Message(ABC):
+    """
+    Abstract base class representing a message in the protocol.
+    """
+
     pass
 
 
 class ProtocolMng(ABC):
     """
-    This abstract class is responsible for handling messages encoded with a specific
-    protocol and updating the machine data model accordingly.
+    Abstract class responsible for handling messages encoded with a specific protocol
+    and updating the machine data model accordingly.
+
+    :ivar _data_model: The machine data model that is updated based on the processed messages.
     """
 
     def __init__(self, data_model: DataModel):
+        """
+        Initializes the ProtocolMng with a specific machine data model.
+
+        :param data_model: The machine data model to be used for updating based on the processed messages.
+        """
+
         self._data_model = data_model
 
     @abstractmethod
     def handle_message(self, msg: Message) -> Message:
         """
-        Abstract method called to handle a message encoded with a specific protocol and
-        update the machine data model accordingly.
+        Abstract method to handle a protocol-specific message and update the
+        machine data model.
+
+        :param msg: The message to be handled, which should be an instance of the appropriate protocol message.
+        :return: A response message based on the handling of the input message.
         """
+
         pass
 
     def get_data_model(self) -> DataModel:
         """
-        Get the machine data model.
+        Returns the machine data model.
+
+        :return: The current machine data model used by the protocol manager.
         """
         return self._data_model
 
     # This class should be responsible for parsing the msgs and calling the
     # respective methods on the machine data model to handle the messages.
-    # Note that the protocol manager is responsible for parsing the messages and creating
-    # the correct response messages.
+    # Note that the protocol manager is responsible for parsing the messages and
+    # creating the correct response messages.
