608c26cc6e7d847bf1bccbcdb453760194e74cda
Enrico Fraccaroli (Galfurian) <enry.frak@gmail.com>
2025-02-17 13:24:20 -0500

    Add comments to all the Payload-related classes.

--- machine_data_model/protocols/glacier_v1/glacier_payload.py
+++ machine_data_model/protocols/glacier_v1/glacier_payload.py
@@ -6,7 +6,12 @@ from enum import Enum
 @dataclass(init=True, slots=True)
 class Payload:
     """
-    This class is an abstract class that represents the payload of a message.
+    Abstract base class for the payload of a message in the Glacier protocol.
+
+    This class holds the general structure for the payload, which can be
+    extended for different types of messages.
+
+    :cvar node: The node associated with the message payload.
     """
 
     node: str = ""
@@ -15,7 +20,13 @@ class Payload:
 @dataclass(init=True, slots=True)
 class VariablePayload(Payload):
     """
-    This class represents the payload of a variable message.
+    Represents the payload of a variable-related message.
+
+    This class extends Payload and includes the value associated with the
+    variable message.
+
+    :cvar node: The node associated with the message payload (inherited).
+    :cvar value: The value of the variable in the message payload.
     """
 
     value: Any = None
@@ -24,7 +35,15 @@ class VariablePayload(Payload):
 @dataclass(init=True, slots=True)
 class MethodPayload(Payload):
     """
-    This class represents the payload of a method message.
+    Represents the payload of a method-related message.
+
+    This class extends Payload and includes arguments, keyword arguments, and
+    return values for method invocations.
+
+    :cvar node: The node associated with the message payload (inherited).
+    :cvar args: The list of arguments for the method.
+    :cvar kwargs: The dictionary of keyword arguments for the method.
+    :cvar ret: The dictionary of return values from the method.
     """
 
     args: list[Any] = field(default_factory=list)
@@ -33,6 +52,18 @@ class MethodPayload(Payload):
 
 
 class ErrorCode(int, Enum):
+    """
+    Enum for error codes used in the Glacier protocol.
+
+    :cvar UNKNOWN: General unknown error.
+    :cvar BAD_REQUEST: The request is invalid.
+    :cvar NOT_FOUND: The requested node or resource was not found.
+    :cvar NOT_ALLOWED: The requested operation is not allowed.
+    :cvar NOT_SUPPORTED: The requested operation is not supported.
+    :cvar NOT_IMPLEMENTED: The requested operation is not implemented.
+    :cvar VERSION_NOT_SUPPORTED: The requested version of the protocol is not supported.
+    """
+
     UNKNOWN = -1
     BAD_REQUEST = 100
     NOT_FOUND = 101
@@ -43,6 +74,18 @@ class ErrorCode(int, Enum):
 
 
 class ErrorMessages(str, Enum):
+    """
+    Enum for error messages associated with the Glacier protocol errors.
+
+    :cvar INVALID_NAMESPACE: Error message for invalid namespace.
+    :cvar INVALID_REQUEST: Error message for invalid request.
+    :cvar NODE_NOT_FOUND: Error message for node not found.
+    :cvar NOT_SUPPORTED: Error message for unsupported operation.
+    :cvar BAD_REQUEST: Error message for bad request.
+    :cvar NOT_ALLOWED: Error message for not allowed operation.
+    :cvar VERSION_NOT_SUPPORTED: Error message for unsupported protocol version.
+    """
+
     INVALID_NAMESPACE = "Invalid namespace"
     INVALID_REQUEST = "Invalid request"
     NODE_NOT_FOUND = "Node not found"
@@ -55,7 +98,13 @@ class ErrorMessages(str, Enum):
 @dataclass(init=True, slots=True)
 class ErrorPayload(Payload):
     """
-    This class represents the payload of an error message.
+    Represents the payload of an error message in the Glacier protocol.
+
+    This class extends Payload and includes error-related information.
+
+    :cvar node: The node associated with the message payload (inherited).
+    :cvar error_code: The error code associated with the error message.
+    :cvar error_message: A description of the error.
     """
 
     error_code: int = -1
