be90bc0726b796a2189fec9fc4433f148b248911
Sebastiano <gaia.franseba@gmail.com>
2026-05-20 19:02:33 +0200

    fix(tests): Update OPC UA test server image to use latest tag

--- tests/nodes/connectors/opcua/conftest.py
+++ tests/nodes/connectors/opcua/conftest.py
@@ -21,8 +21,7 @@ def start_opcua_test_server() -> Generator[tuple[Container, int], Any, None]:
     docker_client = docker.from_env()
     container_guest_port = "50000/tcp"
     container = docker_client.containers.run(
-        "mcr.microsoft.com/iotedge/opc-plc@sha256:"
-        "1fda0e687dee9bd86e1d40ad3e1e4a81d087777d59cab5abd62fdba9c3eefa9e",
+        "mcr.microsoft.com/iotedge/opc-plc:latest",
         "--pn=50000 --autoaccept --sph --sn=5 --sr=10 --st=uint "
         "--fn=5 --fr=1 --ft=uint --gn=5",
         auto_remove=True,
